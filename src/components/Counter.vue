f5cd5406c8cef6b4bd5d0775bc2e4206d0f78c09
krloz1003 <krloz1003@gmail.com>
2018-11-10 23:46:03 -0600

    Iniciando con vuex

---
+++ src/components/Counter.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="col-md-6 col-md-offset3 well">
+        <input class="form-control text-center" type="text" v-model="total" />
+    
+        <hr />
+
+        <button class="btn btn-block btn-info" @click="increment()">
+            Incrementar
+        </button> 
+        <h4 class="text-center">{{ count }}</h4>
+        <button class="btn btn-block btn-info" @click="decrementar()">
+            Decrementar
+        </button> 
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
---
+++ src/main.js
@@ -0,0 +1,33 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+Vue.config.productionTip = false
+
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment (state, payload = 1) {
+      state.count += payload
+    },
+    decrement (state, payload = 1) {
+      state.count -= payload
+    }
+  }
+})
+
+store.commit('increment')
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  store,
+  components: { App },
+  template: '<App/>'
+})
